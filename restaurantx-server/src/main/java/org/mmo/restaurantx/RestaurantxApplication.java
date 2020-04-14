425475a90ee2ab75af46d79fb21c39d0664e4b27
MahmoudMarzouk2020 <MahmoudMarzouk2016@gmail.com>
2020-04-14 02:51:37 +0200

    server app initialization

---
+++ restaurantx-server/src/main/java/org/mmo/restaurantx/RestaurantxApplication.java
@@ -0,0 +1,13 @@
+package org.mmo.restaurantx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantxApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(RestaurantxApplication.class, args);
+    }
+    
+}
---
+++ restaurantx-server/src/test/java/org/mmo/restaurantx/RestaurantxApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mmo.restaurantx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantxApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    }
+    
+}
